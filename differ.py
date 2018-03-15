b46d3814451b842825b2856fb18b34cdf6d55d8e
thamilarasi43 <37330896+thamilarasi43@users.noreply.github.com>
2018-03-15 10:20:10 +0530

    Create differ.py

---
+++ differ.py
@@ -0,0 +1,10 @@
+def nearlyequal(str1,str2):
+  str1=input("firstword")
+  str2=input("secondword")
+  if str1==str2:
+    print(true)
+  else:
+    print(false)
+    return nearlyequal(str1,str2)
+    print(nearlyequal(str1,str2))
+    
