7a6e85d0549a87eb3bb749d65795c5edcc4b0ece
thamilarasi43 <37330896+thamilarasi43@users.noreply.github.com>
2018-03-15 10:49:53 +0530

    Create strconcat.py

---
+++ strconcat.py
@@ -0,0 +1,5 @@
+a=int(input("Enter the first string")
+b=int(input("Enter the second string")
+print('the first string is'a)
+print('the second string is',b)
+print('contatination ofstring',a+b,b+a)
