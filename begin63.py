4c09dd067f0a00115617fb3d22bdcf4792606def
thamilarasi43 <37330896+thamilarasi43@users.noreply.github.com>
2018-03-21 15:03:01 +0530

    Create begin63.py

---
+++ begin63.py
@@ -0,0 +1,11 @@
+str=input()
+char=0
+word=1
+for i in str:
+  char=char+1
+  if(i==' '):
+    word=word+1
+print("Number of words in the string:")
+print(word)
+print("Number of characters in the string:")
+print(char)
